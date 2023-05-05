29913be13251970098f612fd24b42564f69b237b
bluebeaggle <shew1903@gmail.com>
2023-05-05 22:10:12 +0900

    transcription Titanic sencond start without annotation

---
+++ Titanic/Transcription_2-1__Titanic.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+plt.style.use('seaborn')
+sns.set(font_scale=2.5)
+
+import missingno as msno
+
+#ignore warnings
+import warnings
+warnings.filterwarnings('ignore')
+
+# %matplotlib inline
+# 위 구문의 경우 jupyter notebook에서 사용되는 구문이며, matplotlib의 이미지가 브라우져 내부에 자동으로 인라인되게 하는 명령어 이다.
+
+
+
+
