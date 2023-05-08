e8735d6658bcbe9d24f232c064aad791088774e3
bluebeaggle <shew1903@gmail.com>
2023-05-08 13:12:19 +0900

    Titanic emsmble 2nd transcription start

--- Titanic/Titanic_ensemble_2.py
+++ Titanic/Titanic_ensemble_2.py
@@ -19,10 +19,53 @@ from sklearn.model_selection import GridSearchCV, cross_val_score, StratifiedKFo
 sns.set(style='white', context='notebook', palette='deep')
 
 
+print('========================================================S T A R T========================================================')
+print('=========================================================================================================================')
+# Load data
+##### Load train and Test set
+train = pd.read_csv('~/Desktop/Daeheon/ECU-Test/KaggleForMe/Titanic/train.csv')
+test = pd.read_csv('~/Desktop/Daeheon/ECU-Test/KaggleForMe/Titanic/train.csv')
+IDtest = test['PassengerId']
 
 
+# Outlier detection 
 
+def detect_outliers(df,n,features):
+    """
+    Takes a dataframe df of features and returns a list of the indices
+    corresponding to the observations containing more than n outliers according
+    to the Tukey method.
+    """
+    outlier_indices = []
+    
+    # iterate over features(columns)
+    for col in features:
+        # 1st quartile (25%)
+        Q1 = np.percentile(df[col], 25)
+        # 3rd quartile (75%)
+        Q3 = np.percentile(df[col],75)
+        # Interquartile range (IQR)
+        IQR = Q3 - Q1
+        
+        # outlier step
+        outlier_step = 1.5 * IQR
+        
+        # Determine a list of indices of outliers for feature col
+        outlier_list_col = df[(df[col] < Q1 - outlier_step) | (df[col] > Q3 + outlier_step )].index
+        
+        # append the found outlier indices for col to the list of outlier indices 
+        outlier_indices.extend(outlier_list_col)
+        
+    # select observations containing more than 2 outliers
+    outlier_indices = Counter(outlier_indices)        
+    multiple_outliers = list( k for k, v in outlier_indices.items() if v > n )
+    
+    return multiple_outliers   
 
+# detect outliers from Age, SibSp , Parch and Fare
+Outliers_to_drop = detect_outliers(train,2,["Age","SibSp","Parch","Fare"])
+# Show the outliers rows
+print(train.loc[Outliers_to_drop] )
 
 
 
