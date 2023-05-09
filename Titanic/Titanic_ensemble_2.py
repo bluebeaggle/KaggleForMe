64141a55020b9580d54f8b634a9493e8ce5134dd
bluebeaggle <shew1903@gmail.com>
2023-05-10 08:53:07 +0900

    Titanic_ensemble_2

--- Titanic/Titanic_ensemble_2.py
+++ Titanic/Titanic_ensemble_2.py
@@ -23,12 +23,12 @@ print('================S T A R T================')
 print('=========================================')
 # Load data
 ##### Load train and Test set
-# train = pd.read_csv('~/Desktop/Daeheon/ECU-Test/KaggleForMe/Titanic/train.csv')
-# test = pd.read_csv('~/Desktop/Daeheon/ECU-Test/KaggleForMe/Titanic/train.csv')
-# IDtest = test['PassengerId']
-train = pd.read_csv('~/Desktop/Daeheon/conda/KaggleForMe/Titanic/train.csv')
-test = pd.read_csv('~/Desktop/Daeheon/conda/KaggleForMe/Titanic/train.csv')
+train = pd.read_csv('~/Desktop/Daeheon/ECU-Test/KaggleForMe/Titanic/train.csv')
+test = pd.read_csv('~/Desktop/Daeheon/ECU-Test/KaggleForMe/Titanic/train.csv')
 IDtest = test['PassengerId']
+# train = pd.read_csv('~/Desktop/Daeheon/conda/KaggleForMe/Titanic/train.csv')
+# test = pd.read_csv('~/Desktop/Daeheon/conda/KaggleForMe/Titanic/train.csv')
+# IDtest = test['PassengerId']
 
 # Outlier detection 
 # 이상치 감지 - 범위에서 많이 벗어난 값
@@ -110,7 +110,32 @@ print('==============Age vs Survived======================')
 # Explore Age vs Survived
 g = sns.FacetGrid(train, col='Survived')
 g = g.map(sns.histplot, 'Age', kde=True, stat="density", linewidth=1)
+# Age는 가우스 분포도로 보이기도 함
+# 생존한 하위집단과 생존하지 못한 하위집단의 연령분포가 같지 않음.
+# 어린 승객들이 생존할 기회가 더 많아 보임
+print('==============Age Distribution=====================')
+# Explore Age distribution
+g = sns.kdeplot(train['Age'][(train['Survived']==0) & (train['Age'].notnull())], color = 'Red', fill=True)
+g = sns.kdeplot(train['Age'][(train['Survived']==1) & (train['Age'].notnull())], ax=g, color = 'Blue', fill=True)
+g.set_xlabel('Age')
+g.set_ylabel('Frequency')
+g = g.legend(['Not Survived', 'Survived'])
+print('==============Fare Distribution=====================')
+# Explore Fare distrivution
+g = sns.histplot(dataset['Fare'], color='m', label='Skeness : %.2f'%(dataset['Fare'].skew()))
+g = g.legend(loc='best')
+# 편향이 매우 심하므로 log를 취해 줄이기
+# Apply log to Fare to reduce skewness distribution
+dataset['Fare'] = dataset['Fare'].map(lambda i : np.log(i) if i>0 else 0)
+g = sns.histplot(dataset["Fare"], color="b", label="Skewness : %.2f"%(dataset["Fare"].skew()))
+g = g.legend(loc="best")
 
+print('==============Categorical values=====================')
+print('============== S E X =====================')
+g = sns.barplot(x="Sex",y="Survived",data=train)
+g = g.set_ylabel("Survival Probability")
+
+print(train[["Sex","Survived"]].groupby('Sex').mean())
 
 
 
